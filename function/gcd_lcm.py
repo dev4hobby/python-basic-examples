26e0c22330f50afc5293c005c4d69524bde31e33
d3fau1t <aciddust20@gmail.com>
2022-07-09 13:47:02 +0900

    fix: example

--- function/gcd_lcm.py
+++ function/gcd_lcm.py
@@ -14,3 +14,15 @@ func_list = [gcd, lcm]
 
 print(func_list[0](72, 90))  # 최대공약수
 print(func_list[1](8, 10))  # 최소공배수
+
+
+# 최대 약수
+def greatest_divisor(n):
+    for i in range(n-1, 1, -1):
+        if n % i == 0:
+            return i
+    return n
+
+print(greatest_divisor(10))
+print(greatest_divisor(869))
+print(greatest_divisor(2491))
