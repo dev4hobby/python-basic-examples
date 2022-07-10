f4002baa75174773530c1d949bdb1c1817b4a708
d3fau1t <aciddust20@gmail.com>
2022-07-10 13:36:30 +0900

    fix: example without ray

--- multiprocess/no_ray.py
+++ multiprocess/no_ray.py
@@ -1,16 +1,29 @@
 from datetime import datetime
-from random import randint
-arr = [randint(1, 300) for _ in range(100000000)]
-count = 0
+from random import randint, shuffle
+
+dataset_length = 6
+
+arr = [randint(1, 300) for _ in range(10000000)]
+dataset = []
+for i in range(dataset_length):
+  dataset.append(arr.copy())
+  shuffle(dataset[i])
+
 
 limit = len(arr)
 begin = datetime.now()
+result = []
 print(begin)
-while count < limit:
-  temp = sum(arr[count:count+2])/2
-  arr.append(temp)
-  # print(f"({arr[count]} + {arr[count+1]}) / 2 = {temp}")
-  count += 2
+for i in range(dataset_length):
+  count = 0
+  while count < limit:
+    temp = sum(dataset[i][count:count+2])/2
+    dataset[i].append(temp)
+    # print(f"({arr[count]} + {arr[count+1]}) / 2 = {temp}")
+    count += 2
+  result.append(dataset[i])
+  print(f"Epoch {i} Done >> {datetime.now()-begin}")
 
 print(f"Done >> {datetime.now()-begin}")
-print(f"...{arr[-10:]}")
+for i, _ in enumerate(result):
+  print(f"{i}: {_[-10:]}")
